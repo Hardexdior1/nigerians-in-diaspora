7d35243cf79023a126960342648d8cd3999ae38b
adeniran <adeniranquwam001@gmail.com>
2024-08-23 01:27:35 +0100

    new-changes

--- src/Components/Blog.js
+++ src/Components/Blog.js
@@ -80,7 +80,7 @@ const Blog = () => {
 
       
 
-        <section className="p-6 bg-gray-100">
+        <section className="p-4 bg-gray-100">
     <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
       {blog.map((post) => (
       
